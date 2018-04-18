7be82861b0831e7b27d08d0a3d31533ced8a4a11
Marc Beckerle <marc.beckerle@hrblock.com>
2018-04-18 16:38:46 -0500

    v6

--- Aws/PROD/W1/main.tf
+++ Aws/PROD/W1/main.tf
@@ -1,18 +1,28 @@
 # PROVIDER CONFIGURATION
-  provider "aws" {
-  access_key                    = "${var.access_key}"
-  secret_key                    = "${var.secret_key}"
-  region                        = "us-west-1"
-  }
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
+}
+
 # VM CONFIGURATION
-  resource "aws_instance" "web" {
-  ami                           = "ami-493f2f29"
-  instance_type                 = "t2.micro"
-  subnet_id                     = "subnet-1f164c78"
-  vpc_security_group_ids        = ["sg-06515d7f"]
+resource "aws_instance" "web" {
+  ami                    = "ami-493f2f29"
+  instance_type          = "t2.micro"
+  subnet_id              = "subnet-1f164c78"
+  vpc_security_group_ids = ["sg-06515d7f"]
+
   tags {
-  "Identity"                  = "NWI-041818-eagle"
-  "Name"                      = "Marc Beckerle"
-  "Company"                   = "HRB"
+    "Identity" = "NWI-041818-eagle"
+    "Name"     = "Marc Beckerle"
+    "Company"  = "HRB"
   }
-  }
+}
+
+output "public_ip" {
+  value = "${aws_instance.web.public_ip}"
+}
+
+output "public_dns" {
+  value = "${aws_instance.web.public_dns}"
+}
--- Aws/PROD/W1/variables.tf
+++ Aws/PROD/W1/variables.tf
@@ -1,6 +1,11 @@
-  variable "access_key" {
+variable "access_key" {
   description = "Enter Access Key"
-  }
-  variable "secret_key" {
+}
+
+variable "secret_key" {
   description = "Enter Secret Key"
-  }
+}
+
+variable "region" {
+  description = "Default Region"
+}
